6c61da4bd725096e9e590196f377f5817b10b911
Arthur <armilanez@gmail.com>
2022-05-23 14:15:36 -0300

    reseted git to previous version

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-<<<<<<< HEAD
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
-=======
->>>>>>> a710acb7a418faec99f07b404230f5e9b024381f
